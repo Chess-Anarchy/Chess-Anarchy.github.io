da5e9ead38addbc05d3c24c6536d43ec54091e2b
AlvoSil <levkrom2009@gmail.com>
2023-06-08 22:32:42 +0200

    Added posts folder

---
+++ Posts/post_search.js
@@ -0,0 +1,8 @@
+function action(form) {
+    console.log("Post search data submitted");
+  
+    // Additional logic for form submission
+    // ...
+  
+    return false; // Prevent form from actually submitting
+  }
