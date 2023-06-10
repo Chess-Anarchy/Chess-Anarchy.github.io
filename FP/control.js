3ab2a49a4012f40041e90d1794700d98352e2fe0
Brabygg <97975579+Brabygg@users.noreply.github.com>
2023-06-10 20:06:50 +0200

    Made some tweaks in preparation for merge

--- FP/control.js
+++ FP/control.js
@@ -1,5 +1,3 @@
-const topBar = document.getElementById('top_bar');
-
 const openPopupButton = document.getElementById('Home_button');
 const closePopupButton = document.getElementById('closePopup');
 const popupContainer = document.getElementById('popupContainer');
@@ -8,18 +6,6 @@ const intro = document.getElementById("intro")
 const pointer = document.getElementById("pointer")
 const sentence = "Welcome to the website for anarchists with an interest in chess! We have everything you didn't want, need, or even ask for.".split("")
 const sentence2 = "For example, this brickpipi calculator! (kinda broken rn) ->".split("")
-
-
-//Moves the top bar buttons
-document.addEventListener("mouseover", moveButton, false);
-
-function moveButton(e) {
-    var mouseoverElement = e.target
-    if (mouseoverElement.classList.contains('top_bar_button')) {
-        topBar.style.left = (Math.random() * 20 + 40) + '%';
-    }
-}
-
 
 //Handles the popup when pressing the home button
 openPopupButton.addEventListener('click', function() {
